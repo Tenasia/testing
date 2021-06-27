5f3530a8f35681f74bfa666f9917494258eba4f7
Tenasia <alvicviojan@gmail.com>
2021-06-28 02:26:52 +0800

    hello

---
+++ app.js
@@ -0,0 +1,40 @@
+for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+
+// var fizz = true;
+// if (3 % 100) {
+//     console.log("fizz");
+// } else if (5 % 100) {
+//     console.log("buzz");
+// } else if (15 % 100) {
+//     console.log("fizzbuzz");
+// }
+
+
+
+
+// for fizzbuzz in range(100):
+
+
+//     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+//         print("FizzBuzz")
+
+//     elif fizzbuzz % 3 == 0:
+//         print("Fizz")
+//         continue
+//     elif fizzbuzz % 5 == 0:
+//         print("Buzz")
+//         continue
+
+
+//     print(fizzbuzz)
